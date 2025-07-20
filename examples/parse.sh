214b60d52d48aaa2542730bf7800bd122db5f51a
evid <210122431+evidlabel@users.noreply.github.com>
2025-07-20 22:32:16 +0200

    example

--- examples/parse.sh
+++ examples/parse.sh
@@ -1,12 +1,12 @@
 # these are test runs, add --full to get complete datasets
-hudoc -o data_grevio1 -f grevio.rss
-hudoc -o data_commhr1 -f commhr.rss
-hudoc -o data_cpt1    -f hudoccpt.rss
-hudoc -o data_ecri1   -f hudocecri.rss
-hudoc -o data_ecrml1  -f hudocecrml.rss
-hudoc -o data_esc1    -f hudocesc.rss
-hudoc -o data_exec1   -f hudocexec.rss
-hudoc -o data_fcnm1   -f hudocfcnm.rss
-hudoc -o data_greco1  -f hudocgreco.rss
-hudoc -o data_greta1  -f hudocgreta.rss
-hudoc -o data_echr    -f echr.rss 
+hudoc -o data_grevio1  grevio.rss
+hudoc -o data_commhr1  commhr.rss
+hudoc -o data_cpt1     hudoccpt.rss
+hudoc -o data_ecri1    hudocecri.rss
+hudoc -o data_ecrml1   hudocecrml.rss
+hudoc -o data_esc1     hudocesc.rss
+hudoc -o data_exec1    hudocexec.rss
+hudoc -o data_fcnm1    hudocfcnm.rss
+hudoc -o data_greco1   hudocgreco.rss
+hudoc -o data_greta1   hudocgreta.rss
+hudoc -o data_echr     echr.rss 
