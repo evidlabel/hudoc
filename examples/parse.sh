0555a1f174be0e8c09eb00a56ff2643d8233cc65
evid <210122431+evidlabel@users.noreply.github.com>
2025-07-20 18:31:05 +0200

    update

--- examples/parse.sh
+++ examples/parse.sh
@@ -1,13 +1,12 @@
 # these are test runs, add --full to get complete datasets
-
-hudoc -t grevio  -e  -o data_grevio1     -f grevio.rss
-hudoc -t commhr  -e  -o data_commhr1     -f commhr.rss
-hudoc -t cpt     -e  -o data_cpt1        -f hudoccpt.rss
-hudoc -t ecri    -e  -o data_ecri1       -f hudocecri.rss
-hudoc -t ecrml   -e  -o data_ecrml1      -f hudocecrml.rss
-hudoc -t esc     -e  -o data_esc1        -f hudocesc.rss
-hudoc -t exec    -e  -o data_exec1       -f hudocexec.rss
-hudoc -t fcnm    -e  -o data_fcnm1       -f hudocfcnm.rss
-hudoc -t greco   -e  -o data_greco1      -f hudocgreco.rss
-hudoc -t greta   -e  -o data_greta1      -f hudocgreta.rss
-hudoc -t echr    -e  -o data_echr        -f echr.rss 
+hudoc -t grevio -o data_grevio1 -f grevio.rss
+hudoc -t commhr -o data_commhr1 -f commhr.rss
+hudoc -t cpt    -o data_cpt1    -f hudoccpt.rss
+hudoc -t ecri   -o data_ecri1   -f hudocecri.rss
+hudoc -t ecrml  -o data_ecrml1  -f hudocecrml.rss
+hudoc -t esc    -o data_esc1    -f hudocesc.rss
+hudoc -t exec   -o data_exec1   -f hudocexec.rss
+hudoc -t fcnm   -o data_fcnm1   -f hudocfcnm.rss
+hudoc -t greco  -o data_greco1  -f hudocgreco.rss
+hudoc -t greta  -o data_greta1  -f hudocgreta.rss
+hudoc -t echr   -o data_echr    -f echr.rss 
